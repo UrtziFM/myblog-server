cf1ddc477df8c52a955fa817053b5ea7fecf4a98
UrtziFM <urtzi.arana13@gmail.com>
2020-06-14 08:41:12 +0200

    I have found out the problem, it was in access...

--- config/passport.js
+++ config/passport.js
@@ -13,7 +13,6 @@ passport.use(
   new LocalStrategy(
     { usernameField: 'email', passwordField: 'password', session: false },
     (email, password, done) => {
-      console.log('register email')
       User.findOne({ email })
         .then(user => {
           if (user) {
@@ -22,13 +21,11 @@ passport.use(
           }
 
           bcrypt.genSalt(12, function(err, salt) {
-            console.log(salt)
             if (err) {
               return done(err, null)
             }
 
             bcrypt.hash(password, salt, (hashError, encryptedPass) => {
-              console.log(encryptedPass)
               if (hashError) {
                 return done(hashError, null)
               }
@@ -55,25 +52,19 @@ passport.use(
   new LocalStrategy(
     { usernameField: 'email', passwordField: 'password', session: false },
     (email, password, done) => {
-      console.log('login email')
       User.findOne({ email })
         .then(user => {
           if (!user) {
             const err = new Error('No user found with this email')
             return done(err, null)
           }
-          console.log('fuck')
           bcrypt.compare(password, user.password, (err, isMatch) => {
-            console.log(password)
-            console.log(user.password)
             if (err || !isMatch) {
               const matchError = new Error('Incorrect email or password')
               return done(matchError, null)
             }
 
             jwt.sign({ id: user._id }, jwtSecret, { expiresIn: 36000 }, (jwtErr, token) => {
-              console.log(jwtSecret)
-              console.log(token)
               if (jwtErr || !token) {
                 const error = new Error('There was an unexpected error, try again later')
                 return done(error, null)
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,9 +2,7 @@ const passport = require('passport')
 
 const login = (req, res, next) => {
   passport.authenticate('login', (err, token) => {
-    console.log('it is you')
     if (err || !token) {
-      console.log('error in log')
       const error = new Error('There was an error login in')
       return next(error)
     }
@@ -18,19 +16,16 @@ const login = (req, res, next) => {
 
 const register = (req, res, next) => {
   passport.authenticate('register', (err, user) => {
-    console.log('Hello')
     if (err || !user) {
       const error = new Error(err ? err.message : 'There was an error creating the user')
       return next(error)
     }
-    console.log('Hello1')
 
     login(req, res, next)
   })(req, res, next)
 }
 
 const isLoggedIn = (req, res, next) => {
-  console.log('failed')
   res.status(200).json('User is logged in')
   return
 }
