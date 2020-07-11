4da843339738778df010ec96b11397d8919afd09
UrtziFM <urtzi.arana13@gmail.com>
2020-07-11 14:52:45 +0200

    I am blocked with CORS

--- app.js
+++ app.js
@@ -31,17 +31,13 @@ hbs.registerHelper('json', context => {
   return JSON.stringify(context)
 })
 
-//CORS middleware
-var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'content-type');
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,");
   next();
-}
+});
 
-app.use(cors());
-app.use(allowCrossDomain);
+//app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
